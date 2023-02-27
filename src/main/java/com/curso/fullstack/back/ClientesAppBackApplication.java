0883e62ea649ca792aef54d4ee1974f352aaf0fb
luisfer19972020 <lcorrea.softsol@gmail.com>
2023-02-26 20:30:41 -0600

    Avance hasta el video 37

--- src/main/java/com/curso/fullstack/back/ClientesAppBackApplication.java
+++ src/main/java/com/curso/fullstack/back/ClientesAppBackApplication.java
@@ -2,9 +2,9 @@ package com.curso.fullstack.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+
+@SpringBootApplication
 public class ClientesAppBackApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/curso/fullstack/back/models/entity/Cliente.java
@@ -0,0 +1,69 @@
+package com.curso.fullstack.back.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
