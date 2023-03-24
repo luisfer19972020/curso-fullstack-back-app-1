309b3cd9769664c331d4911026d73ba9e9856c8c
luisfer19972020 <lcorrea.softsol@gmail.com>
2023-03-23 21:23:53 -0600

    Avance hasta video 91

--- src/main/java/com/curso/fullstack/back/models/entity/Cliente.java
+++ src/main/java/com/curso/fullstack/back/models/entity/Cliente.java
@@ -29,7 +29,7 @@ public class Cliente implements Serializable {
     @Column(nullable = false)
     @NotBlank
     private String apellido;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = false)
     @Email
     private String email;
     @Column(name = "created_at")
